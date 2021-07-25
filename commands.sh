0d0c2987e8cebed20aef4476722941f1c9513348
unknown <nicholasklaene@outlook.com>
2021-07-25 19:42:57 -0400

    aws stuff

--- commands.sh
+++ commands.sh
@@ -2,7 +2,10 @@
 echo "Starting build!"
 
 # Install Node.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
 . ~/.nvm/nvm.sh
 nvm install node
 
