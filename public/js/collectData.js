8b1557e9aa290ca41c471564cb82d09f5e481398
unknown <nicholasklaene@outlook.com>
2021-07-25 20:46:49 -0400

    save actions to localstorage

---
+++ public/js/collectData.js
@@ -0,0 +1,42 @@
+let timeLoaded = performance.now();
+
+let path = window.location.pathname;
+let pageNumber = path.substring(path.indexOf("/") + 1, path.indexOf("."));
+
+let actionNumber;
+if (pageNumber == 12) actionNumber = 1;
+else if (pageNumber == 15) actionNumber = 2;
+else actionNumber = 3;
+
+let answerSelected = false;
+
+// Need nextPageBtn id on every page
+const nextPageBtn = document.getElementById("nextPageBtn");
+const options = document.querySelectorAll(".option a");
+
+nextPageBtn.addEventListener("click", (e) => {
+  if (!answerSelected) {
+    e.preventDefault();
+    return;
+  }
+  let timeFinished = performance.now();
+  localStorage.setItem(`action${actionNumber}t`, timeFinished / 1000);
+});
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let clicked = option.getAttribute("value");
+    option.classList.add("selected-option");
+    document.querySelectorAll(".option a").forEach((otherOpt) => {
+      if (
+        otherOpt.classList.contains("selected-option") &&
+        otherOpt != option
+      ) {
+        otherOpt.classList.remove("selected-option");
+      }
+    });
+    localStorage.setItem(`action${actionNumber}a`, clicked);
+    answerSelected = true;
+    nextPageBtn.style.cursor = "pointer";
+  });
+});
